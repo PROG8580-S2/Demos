bbd104b8b6358505c037ce614d27e35dedff3800
Tony Theodoropoulos <theodot@outlook.com>
2023-05-10 08:07:39 -0400

    Week 1 Demos complete

---
+++ Week1Demos/ComputeExpression.java
@@ -0,0 +1,11 @@
+
+public class ComputeExpression {
+
+	public static void main(String[] args) {
+		System.out.print("3.5 * 4 / 2 - 2.5 = ");
+		System.out.println(3.5 * 4 / 2 - 2.5);
+
+		System.out.print("35 C in F is ");
+		System.out.println((9.0 / 5) * 35 + 32);
+	}
+}
---
+++ Week1Demos/WelcomeMessage.java
@@ -0,0 +1,10 @@
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to PROG8580");
+		System.out.println();
+		System.out.println("This is week 1");
+	}
+
+}
